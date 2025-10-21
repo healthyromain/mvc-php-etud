d0d46bb2fcfd088ddf3ee6e925d74e6ba06c0d7f
Lecrot Gaulier Romain <73986902+healthyromain@users.noreply.github.com>
2025-10-21 09:44:23 +0200

    Créez un template et un layout

--- PHP-MVC-P1/code/templates/homepage.php
+++ PHP-MVC-P1/code/templates/homepage.php
@@ -1,32 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>Le blog de l'AVBN</title>
-    <link href="style.css" rel="stylesheet" />
-</head>
-<body>
-    <h1>Le super blog de l'AVBN !</h1>
-    <p>Derniers billets du blog :</p>
+<?php $title = "Le blog de l'AVBN"; ?>
 
-    <?php if (!empty($posts) && is_array($posts)): ?>
-        <?php foreach ($posts as $post): ?>
-            <div class="news">
-                <h3>
-                    <?= htmlspecialchars($post['title']) ?>
-                    <em>le <?= $post['french_creation_date'] ?> </em>
-                </h3>
-                <p>
-                    <?= nl2br(htmlspecialchars($post['content'])) ?>
-                    <br />
-                    <em>
-                        <a href="../post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a>
-                    </em>
-                </p>
-            </div>
-        <?php endforeach; ?>
-    <?php else: ?>
-        <p>Aucun billet trouvé.</p>
-    <?php endif; ?>
-</body>
-</html>
+<?php ob_start(); ?>
+<h1>Le super blog de l'AVBN !</h1>
+<p>Derniers billets du blog :</p>
+
+<?php foreach ($posts as $post): ?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($post['title']); ?>
+            <em>le <?= $post['french_creation_date']; ?> </em>
+        </h3>
+        <p>
+            <?= nl2br(htmlspecialchars($post['content'])); ?>
+            <br />
+            <em>
+                <a href="post.php?id=<?= urlencode($post['identifier']) ?>">
+                    Commentaires
+                </a>
+            </em>
+        </p>
+    </div>
+<?php endforeach; ?>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('layout.php'); ?>
--- PHP-MVC-P1/code/templates/post.php
+++ PHP-MVC-P1/code/templates/post.php
@@ -1,29 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>Le blog de l'AVBN</title>
-    <link href="blog/style.css" rel="stylesheet" />
-</head>
-<body>
-    <h1>Le super blog de l'AVBN !</h1>
-    <p><a href="blog/index.php">Retour à la liste des billets</a></p>
+<?php $title = "Billet - " . htmlspecialchars($post['title']); ?>
 
-    <div class="news">
-        <h3>
-            <?= htmlspecialchars($post['title']) ?>
-            <em>le <?= $post['french_creation_date'] ?> </em>
-        </h3>
-        <p><?= nl2br(htmlspecialchars($post['content'])) ?></p>
-    </div>
+<?php ob_start(); ?>
+<h1>Le super blog de l'AVBN !</h1>
+<p><a href="blog/index.php">Retour à la liste des billets</a></p>
 
-    <h2>Commentaires</h2>
-    <?php foreach ($comments as $comment): ?>
-        <p>
-            <strong><?= htmlspecialchars($comment['author']) ?></strong>
-            le <?= $comment['french_creation_date'] ?>
-        </p>
-        <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-    <?php endforeach; ?>
-</body>
-</html>
+<div class="news">
+    <h3>
+        <?= htmlspecialchars($post['title']) ?>
+        <em>le <?= $post['french_creation_date'] ?> </em>
+    </h3>
+    <p><?= nl2br(htmlspecialchars($post['content'])) ?></p>
+</div>
+
+<h2>Commentaires</h2>
+<?php foreach ($comments as $comment): ?>
+    <p>
+        <strong><?= htmlspecialchars($comment['author']) ?></strong>
+        le <?= $comment['french_creation_date'] ?>
+    </p>
+    <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+<?php endforeach; ?>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('layout.php'); ?>
